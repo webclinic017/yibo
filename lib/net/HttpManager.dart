91b236840520ffe227cca808939b19613c3ee905
903345072 <903345072@qq.com>
2021-06-02 18:02:39 +0800

    das

--- lib/net/HttpManager.dart
+++ lib/net/HttpManager.dart
@@ -19,8 +19,8 @@ class HttpManager {
     if (null == _dio) {
       _dio = new Dio(
           new BaseOptions(baseUrl: Address.BASE_URL, connectTimeout: 35000));
-     _dio.interceptors.add(new DioLogInterceptor());
-     _dio.interceptors.add(new PrettyDioLogger());
+     //_dio.interceptors.add(new DioLogInterceptor());
+     //_dio.interceptors.add(new PrettyDioLogger());
       _dio.interceptors.add(new ResponseInterceptors());
     }
   }
